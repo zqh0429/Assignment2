1a97e53ab0e745fd94a9294a465b22853e2cf9f3
zqh0429 <122677348+zqh0429@users.noreply.github.com>
2023-04-24 20:08:42 +0800

    2023/4/24

--- chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Main.java
+++ chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Main.java
@@ -1,23 +1,63 @@
 package cn.edu.sustech.cs209.chatting.client;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch();
-    }
+  Socket socket;
+  String name;
+  BufferedReader in;
+  PrintWriter out;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
-        stage.setScene(new Scene(fxmlLoader.load()));
-        stage.setTitle("Chatting Client");
-        stage.show();
-    }
+
+  public static void main(String[] args) {
+    launch();
+  }
+
+  @Override
+  public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+    VBox pane = fxmlLoader.load();
+    Controller controller = fxmlLoader.getController();
+    name = controller.username;
+    socket = Controller.socket;
+    stage.setScene(new Scene(pane));
+    stage.setTitle("Chatting Client");
+    stage.setOnCloseRequest(event -> {
+      try {
+        socket.close();
+      } catch (IOException e) {
+        throw new RuntimeException();
+      }
+    });
+    stage.show();
+  }
+
+  public Socket getSocket() {
+    return socket;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public BufferedReader getIn() {
+    return in;
+  }
+
+  public PrintWriter getOut() {
+    return out;
+  }
 }
