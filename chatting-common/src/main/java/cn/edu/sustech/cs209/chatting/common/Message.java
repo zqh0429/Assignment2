0b16d53637e221460ad1defc28e2bfe154726ce8
zqh0429 <122677348+zqh0429@users.noreply.github.com>
2023-04-24 20:06:49 +0800

    2023/4/24

--- chatting-common/src/main/java/cn/edu/sustech/cs209/chatting/common/Message.java
+++ chatting-common/src/main/java/cn/edu/sustech/cs209/chatting/common/Message.java
@@ -2,34 +2,34 @@ package cn.edu.sustech.cs209.chatting.common;
 
 public class Message {
 
-    private Long timestamp;
+  private String timestamp;
 
-    private String sentBy;
+  private String sentBy;
 
-    private String sendTo;
+  private String sendTo;
 
-    private String data;
+  private String data;
 
-    public Message(Long timestamp, String sentBy, String sendTo, String data) {
-        this.timestamp = timestamp;
-        this.sentBy = sentBy;
-        this.sendTo = sendTo;
-        this.data = data;
-    }
+  public Message(String timestamp, String sentBy, String sendTo, String data) {
+    this.timestamp = timestamp;
+    this.sentBy = sentBy;
+    this.sendTo = sendTo;
+    this.data = data;
+  }
 
-    public Long getTimestamp() {
-        return timestamp;
-    }
+  public String getTimestamp() {
+    return timestamp;
+  }
 
-    public String getSentBy() {
-        return sentBy;
-    }
+  public String getSentBy() {
+    return sentBy;
+  }
 
-    public String getSendTo() {
-        return sendTo;
-    }
+  public String getSendTo() {
+    return sendTo;
+  }
 
-    public String getData() {
-        return data;
-    }
+  public String getData() {
+    return data;
+  }
 }
